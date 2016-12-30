fedfe5f6250063f79e8762d89a97315fd7cdd186
Elena Zarkova <ezarkova@gmail.com>
2016-12-30 16:53:31 +0200

    Meal-data by page bug fixed

--- server/routers/meals-router.js
+++ server/routers/meals-router.js
@@ -4,7 +4,7 @@
 module.exports = ({ app, controllers, passport }) => {
     const controller = controllers.meals;
 
-    //app.get('/api/meals', controller.getAll);
+    // app.get('/api/meals', controller.getAll);
     app.get('/api/meals', controller.getOnePage);
     app.put('/api/meals/like', passport.authenticate('jwt', { session: false }), controller.likeMeal);
     app.put('/api/meals/dislike', passport.authenticate('jwt', { session: false }), controller.dislikeMeal);
